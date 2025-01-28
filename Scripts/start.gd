47c2514ade05d5a750810c1d1d3509778dc4f22d
VonBryan <vonbanalbryan18v@gmail.com>
2025-01-28 15:53:33 +0800

    Creating Setting UI

--- Scripts/start.gd
+++ Scripts/start.gd
@@ -9,7 +9,7 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world_map.tscn")
 
 func _on_setting_pressed():
-	pass
+	$SETTING.show()
 
 func _on_exit_pressed():
 	get_tree().quit()
